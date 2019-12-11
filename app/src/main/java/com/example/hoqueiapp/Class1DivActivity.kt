5ec136e670083f5d4a2ad6e2d0cafe0a14f838c1
Brunofgm7 <bruno7moreira@gmail.com>
2019-12-11 20:42:00 +0000

    implementação das layouts de quase todas as pag

---
+++ app/src/main/java/com/example/hoqueiapp/Class1DivActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hoqueiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Class1DivActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class1_div)
+    }
+}
---
+++ app/src/main/java/com/example/hoqueiapp/DivisoesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hoqueiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DivisoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_divisoes)
+    }
+}
