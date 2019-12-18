9fafe954cd06cb0494f56c0fabc0e8e89275a7e1
Bruno Moreira <bruno7moreira@gmail.com>
2019-12-18 17:15:14 +0000

    1/2 implementacao do login

--- app/src/main/java/com/example/hoqueiapp/LoginActivity.kt
+++ app/src/main/java/com/example/hoqueiapp/LoginActivity.kt
@@ -3,33 +3,64 @@ package com.example.hoqueiapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     lateinit var BotaoLogin: Button
     lateinit var BotaoBack: Button
-    lateinit var textUserLogin: EditText
+    lateinit var BotaoRegisto: Button
+    lateinit var textEmailLogin: EditText
     lateinit var textPassLogin: EditText
     lateinit var gv: VariaveisGlobais
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         gv = getApplication() as VariaveisGlobais
         setContentView(R.layout.activity_login)
+
         BotaoLogin = findViewById(R.id.BotaoLogin)
-        textUserLogin = findViewById(R.id.textUserLogin)
-        // var a: String = textUserLogin.text.toString()
-        textPassLogin = findViewById(R.id.textPassLogin)
-        // var s: String = textPassLogin.text.toString()
+        BotaoLogin.setOnClickListener(View.OnClickListener {
+            view -> login()
+        })
         BotaoBack = findViewById(R.id.BotaoBack)
         BotaoBack.setOnClickListener {
             executarOutraActivity(MainActivity::class.java)
+        }
+        BotaoRegisto = findViewById(R.id.BotaoRegisto)
+        BotaoRegisto.setOnClickListener {
+            executarOutraActivity(RegistoActivity::class.java)
         }
 
     }
     private fun executarOutraActivity(outraActivity: Class<*>) {
         val x = Intent(this, outraActivity)
         startActivity(x)
+    }
+
+    private fun login () {
+        textEmailLogin = findViewById(R.id.textEmailLogin)
+        var email: String = textEmailLogin.text.toString()
+        textPassLogin = findViewById(R.id.textPassLogin)
+        var pass: String = textPassLogin.text.toString()
+
+        if (!email.isEmpty() && !pass.isEmpty()) {
+            mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this, OnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, LoginOnMainActivity::class.java))
+                    Toast.makeText(this, "Login com sucesso", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Login sem sucesso ", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        } else {
+            Toast.makeText(this, "Por favor preencha os campos!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/hoqueiapp/RegistoActivity.kt
+++ app/src/main/java/com/example/hoqueiapp/RegistoActivity.kt
@@ -5,21 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistoActivity : AppCompatActivity() {
     lateinit var BotaoRegisto: Button
     lateinit var BotaoBack: Button
     lateinit var textUserRegisto: EditText
+    lateinit var textEmailRegisto: EditText
     lateinit var textPassRegisto: EditText
     lateinit var gv: VariaveisGlobais
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         gv = getApplication() as VariaveisGlobais
         setContentView(R.layout.activity_registo)
-        textUserRegisto = findViewById(R.id.textUserRegisto)
-        textPassRegisto = findViewById(R.id.textPassRegisto)
         BotaoRegisto = findViewById(R.id.BotaoRegisto)
+        BotaoRegisto.setOnClickListener {
+            view -> register ()
+        }
         BotaoBack = findViewById(R.id.BotaoBack)
         BotaoBack.setOnClickListener {
             executarOutraActivity(MainActivity::class.java)
@@ -29,5 +35,28 @@ class RegistoActivity : AppCompatActivity() {
     private fun executarOutraActivity(outraActivity: Class<*>) {
         val x = Intent(this, outraActivity)
         startActivity(x)
+    }
+
+    private fun register () {
+        textUserRegisto = findViewById(R.id.textUserRegisto)
+        textEmailRegisto = findViewById(R.id.textEmailRegisto)
+        textPassRegisto = findViewById(R.id.textPassRegisto)
+
+        var user = textUserRegisto.text.toString()
+        var email = textEmailRegisto.text.toString()
+        var pass = textPassRegisto.text.toString()
+
+        if (!user.isEmpty() && !email.isEmpty() && !pass.isEmpty()) {
+            mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, OnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                } else {
+                    Toast.makeText(this, "Registo sem sucesso", Toast.LENGTH_SHORT).show()
+                }
+            })
+        } else {
+            Toast.makeText(this, "Por favor preencha os campos!", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
